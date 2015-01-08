78f3118d95c669f89c40c4b8d84f7435f57c8f25
Milam <charles.milam@gmail.com>
2015-01-07 21:17:51 -0600

    Modify to collect data on hot/cold hands.

--- yaw-bot.js
+++ yaw-bot.js
@@ -32,13 +32,36 @@
         console.log(didIWin);
         console.log(loot[1])
         console.log(loot[0]["value"]);
-        var opPlus, opMinus, opNeutral;
-        var mePlus, meMinus, meNeutral;
+        var oppPlus = 0;
+        var oppMinus = 0;
+        var oppNeutral = 0;
+        var mePlus = 0;
+        var meMinus = 0;
+        var meNeutral = 0;
 
-        // opponent's values
+        // yaw's values
         switch (loot[1].isMine) {
           case loot[1].value < 5:
-            console.log("in < 5 value");
+            meMinus += 1;
+            break;
+          case loot[1].value >= 5 && loot[1].value < 10:
+            meNeutral += 1;
+            break;
+          case loot[1].value > 9:
+            mePlus += 1;
+            break;
+        }
+
+        // oppponent's values
+        switch (loot[0].isMine === "undefined"){
+            case loot[0].value < 5:
+            oppMinus += 1;
+            break;
+          case loot[0].value >= 5 && loot[0].value < 10:
+            oppNeutral += 1;
+            break;
+          case loot[0].value > 9:
+            oppPlus += 1;
             break;
         }
       }
